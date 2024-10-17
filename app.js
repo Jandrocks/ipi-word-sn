22020d9b02ba2bcb87e7de5829f4954527fbf88e
Jandrocks <32751139+Jandrocks@users.noreply.github.com>
2024-10-17 09:53:02 -0300

    servidor replica

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ app.use(express.json());
 
 app.use('/generate-word', documentRoutes);
 
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`API escuchando en http://164.77.34.2:${PORT}`);
+});
+
+/*
 app.listen(PORT, () => {
     console.log(`API escuchando en http://localhost:${PORT}`);
 });
+*/
